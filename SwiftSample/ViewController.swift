5b4755623009349c4362334e673bd4ea2a383174
Sven Herzberg <sven.herzberg@cluepunk.com>
2019-10-21 11:05:09 +0200

    ⭐️ Provide Default UI for Swift Demo

--- SwiftSample/ViewController.swift
+++ SwiftSample/ViewController.swift
@@ -5,13 +5,22 @@
 //  Created by Sven Herzberg on 18.10.19.
 //
 
+import AditionAdsLib
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var aditionLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if let version = AdsView.sdkVersionNumber() {
+            aditionLabel.text = "Using Adition SDK v\(version)"
+        } else {
+            aditionLabel.text = "Adition SDK not found"
+        }
     }
 
 
